dd9e3c64aba58dd3af55e2aa726b0ff5052d6b98
Jamon Holmgren <jamon@clearsightstudio.com>
2013-05-24 00:03:26 -0700

    Adding new 0.7 features. Pending Pull Request.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,20 @@
 class AppDelegate < PM::Delegate
+  
+  status_bar false, animation: :none
 
   def on_load(app, options)
+    register_for_notifications :all
+    
     open_tab_bar HomeScreen, StatesScreen, ContactScreen, HelpScreen.new(nav_bar: true)
+  end
+  
+  # Push Notifications (PM version 0.7+)
+  def on_notification(notification)
+    PM.logger.info notification
+  end
+  
+  def on_registration(token, error)
+    # Push up token to your server here
   end
 
 end
