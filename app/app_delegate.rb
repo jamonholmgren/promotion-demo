89d518a9bb735e3a71858f3f97f55a4e68d04f15
Jamon Holmgren <jamon@clearsightstudio.com>
2013-05-30 09:44:43 -0700

    Fixed issue #104

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,18 +3,19 @@ class AppDelegate < PM::Delegate
   status_bar false, animation: :none
 
   def on_load(app, options)
-    register_for_notifications :all
+    register_for_push_notifications :all
     
     open_tab_bar HomeScreen, StatesScreen, ContactScreen, HelpScreen.new(nav_bar: true)
   end
   
   # Push Notifications (PM version 0.7+)
-  def on_notification(notification)
+  def on_push_notification(notification)
     PM.logger.info notification
   end
   
-  def on_registration(token, error)
+  def on_push_registration(token, error)
     # Push up token to your server here
+
   end
 
 end
