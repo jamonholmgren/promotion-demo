ae8f632dbbe62a2b383b6709d00ff2e556375f73
Jamon Holmgren <jamon@clearsightstudio.com>
2013-06-25 22:01:46 -0700

    Added tab switching

--- app/screens/help_screen.rb
+++ app/screens/help_screen.rb
@@ -4,10 +4,13 @@ class HelpScreen < PM::GroupedTableScreen
   def table_data
     @help_table_data ||= [{
       title: "Get Help",
-      cells: [{
-        title: "Email us", action: :email_us,
-        title: "Open Modal", action: :modal_tapped
-      }]
+      cells: [
+        { title: "Email us", action: :email_us },
+        { title: "Open Modal", action: :modal_tapped },
+        { title: "Home Tab", action: :tab_open, arguments: 0 },
+        { title: "States Tab", action: :tab_open, arguments: 1 },
+        { title: "Contact Tab", action: :tab_open, arguments: "Contact" }
+      ]
     }]
   end
 
@@ -22,6 +25,10 @@ class HelpScreen < PM::GroupedTableScreen
   
   def on_return(args={})
     PM.logger.info args
+  end
+  
+  def tab_open(args)
+    open_tab args
   end
   
 end
